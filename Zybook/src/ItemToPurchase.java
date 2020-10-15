c01b0574a00caadc76bf6081ff1415cb0ce6fc0b
RAZER <RAZER@LAPTOP-D3LV3B4T>
2020-10-16 00:42:26 +0800

    Added all required instance variables

--- Zybook/src/ItemToPurchase.java
+++ Zybook/src/ItemToPurchase.java
@@ -1,5 +1,18 @@
 
 public class ItemToPurchase {
+    /*
+     * Private fields
+String itemName - Initialized in default constructor to "none"
+int itemPrice - Initialized in default constructor to 0
+int itemQuantity - Initialized in default constructor to 0
+     */
     
-
+    String itemName;
+    int itemPrice;
+    int itemQuantity;
+    
+    public static void main(String[] args) {
+	ItemToPurchase it = new ItemToPurchase();
+    }
+    
 }
