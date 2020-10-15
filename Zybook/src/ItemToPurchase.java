6708ad6294531b77435b0b11abcc8b5fbd76ed6c
RAZER <RAZER@LAPTOP-D3LV3B4T>
2020-10-16 01:16:11 +0800

    getter and setter

--- Zybook/src/ItemToPurchase.java
+++ Zybook/src/ItemToPurchase.java
@@ -1,18 +1,30 @@
 
 public class ItemToPurchase {
-    /*
-     * Private fields
-String itemName - Initialized in default constructor to "none"
-int itemPrice - Initialized in default constructor to 0
-int itemQuantity - Initialized in default constructor to 0
-     */
     
     String itemName;
     int itemPrice;
     int itemQuantity;
-    
-    public static void main(String[] args) {
-	ItemToPurchase it = new ItemToPurchase();
+    public String getItemName() {
+        return itemName;
     }
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    public int getItemPrice() {
+        return itemPrice;
+    }
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+    
+    
+    
+   
     
 }
